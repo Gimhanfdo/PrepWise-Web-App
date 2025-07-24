e9eefde78e5a864bd9c02cc4acd0eb66fc1ed415
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-07-24 14:22:57 +0530

    line 25 commented

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
 
 //API Endpoints
-app.get('/', (req, res) =>res.send("API Working"));
+// app.get('/', (req, res) =>res.send("API Working"));
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 app.use('/api/analyze', analysisRouter);
