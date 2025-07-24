299a730e6356a56ce6d85eb53849fc2b3eebfb43
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-07-24 12:32:34 +0530

    update server.js

--- server/server.js
+++ server/server.js
@@ -6,14 +6,17 @@ import connectDB from './config/mongodb.js';
 import authRouter from './routes/authRoutes.js';
 import userRouter from './routes/userRoutes.js';
 import analysisRouter from './routes/analysisRoutes.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-
 const port = process.env.PORT || 4000
+
 connectDB();
 
 const allowedOrigins = [process.env.CLIENT_URL || 'http://localhost:5173']
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
@@ -23,5 +26,11 @@ app.get('/', (req, res) =>res.send("API Working"));
 app.use('/api/auth', authRouter)
 app.use('/api/user', userRouter)
 app.use('/api/analyze', analysisRouter);
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 app.listen(port, ()=> console.log(`Server started on PORT: ${port}`));
