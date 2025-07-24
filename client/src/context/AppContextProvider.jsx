037898b0f5c304b07beb4fc48eab93fff9fca2cb
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-07-25 04:51:39 +0530

    update backendUrl variable in line 10

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -7,7 +7,7 @@ export const AppContextProvider = (props)=>{
 
     axios.defaults.withCredentials = true;
     
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL || 'http://localhost:4000';
     const [isLoggedin, setIsLoggedin] = useState(false)
     const [userData, setUserData] = useState(false)
 
