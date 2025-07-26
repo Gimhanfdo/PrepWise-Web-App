8faf6191f95c7e227325742f5e64a2a52f35574a
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-07-26 13:30:51 +0530

    update register path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
-import Register from './pages/Register'
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import TrainerDashboard from './pages/TrainerDashboard';
 import FresherDashboard from './pages/FresherDashboard'
 import ProfileSettings from './pages/ProfileSettings'
 import CVAnalyzer from './pages/CVAnalyzer'
+import SignUp from './pages/SignUp'
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
       <ToastContainer/>
       <Routes>
         <Route path='/' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
+        <Route path='/register' element={<SignUp/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/email-verify' element={<EmailVerify/>}/>
         <Route path='/reset-password' element={<ResetPassword/>}/>
