bd2a3ef4e5dd1585a124c7acd2bac6ea9c75410f
Gimhan Fernando <gimhanfdo2004@gmail.com>
2025-07-26 13:28:27 +0530

    update analysisRoutes

--- server/routes/analysisRoutes.js
+++ server/routes/analysisRoutes.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import multer from 'multer';
-import { analyzeResume } from '../controllers/analysiscontroller.js';
+import { analyzeResume, deleteAnalysis, getSavedAnalysis, saveAnalysis } from '../controllers/analysiscontroller.js';
 import userAuth from '../middleware/userAuth.js';
 
 const analysisRouter = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 analysisRouter.post('/analyze-resume', userAuth, upload.single('resume'), analyzeResume);
+analysisRouter.post('/save', userAuth, saveAnalysis);
+analysisRouter.get('/saved', userAuth, getSavedAnalysis);
+analysisRouter.delete('/delete/:id', userAuth, deleteAnalysis);
 
 export default analysisRouter;
